67d9f39cd99b4e6a8ee09481ba99a6bede6f104c
boluwatifecaleb <75724982+boluwatifecaleb@users.noreply.github.com>
2026-08-25 07:19:35 +0100

    Initial commit: project setup, DB connection

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+app.use(express.json());
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error('MongoDB connection failed:', error.message);
+        process.exit(1);
+    }
+};
+
+app.get('/', (req, res) => {
+    res.send('API is running ...')
+});
+
+const startServer = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+startServer();
