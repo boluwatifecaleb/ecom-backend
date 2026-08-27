ef16d3375eb09edbb4d4e57aae4b3dcd00de2497
boluwatifecaleb <75724982+boluwatifecaleb@users.noreply.github.com>
2026-08-28 00:04:55 +0100

    feat: add user registration route with password hashing

---
+++ controllers/authController.js
@@ -0,0 +1,24 @@
+import User from '../models/users.js';
+
+export const registerUser = async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+
+        const existingUser = await User.findOne({email});
+        if (existingUser) {
+            return res.status(400).json({ message: "email already in use"});
+        }
+
+        const user = await User.create({name, email, password});
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error",
+            error: error.message});
+    } 
+};
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import authroutes from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 app.use(express.json());
+app.use('/api/auth', authroutes);
 
 const connectDB = async () => {
     try {
